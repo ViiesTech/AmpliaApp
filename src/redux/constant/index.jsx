2a2088195cd62c15aa351c8eb62e20bfb93bda76
SyedHamzaHussain000 <syedhamza0015@gmail.com>
2026-03-12 01:29:01 +0500

    calculator

--- src/redux/constant/index.jsx
+++ src/redux/constant/index.jsx
@@ -1,3 +1,4 @@
+// export const BASE_URL = 'https://apiforapp.link/Amplia/';
 export const BASE_URL = 'https://megasporic-carolyn-diametrically.ngrok-free.dev/';
 export const IMAGE_BASE_URL = `${BASE_URL}uploads/`;
 
